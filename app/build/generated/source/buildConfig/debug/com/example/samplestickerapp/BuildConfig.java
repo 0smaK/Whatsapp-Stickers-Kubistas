cc923da9ea238455ef9cf40c2dbe224231653f3f
Óscar Muñoz <osmak@protonmail.com>
2018-11-15 18:27:11 +0100

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/example/samplestickerapp/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.samplestickerapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.osmak.stickerskubismo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "2.0";
+  // Fields from default config.
+  public static final String CONTENT_PROVIDER_AUTHORITY = "io.osmak.stickerskubismo.stickercontentprovider";
+}
